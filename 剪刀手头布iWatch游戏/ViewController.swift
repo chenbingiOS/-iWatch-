39caa47e75d97adea0a5b672a1da072a6f6c4351
陈冰 <chenbing@wencongkeji.local>
2015-04-24 14:26:05 +0800

    Initial Commit

---
+++ 剪刀手头布iWatch游戏/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  剪刀手头布iWatch游戏
+//
+//  Created by 陈冰 on 15/4/24.
+//  Copyright (c) 2015年 Glacier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
